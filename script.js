d685108e40b8a97616056f898fc4d4466dc307fa
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-08-20 13:44:43 -0300

    Add button for the number of squares per side

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const container = document.querySelector("#container");
 
-function createSquares() {
-    for (let i = 1; i <= 256; i++) {
+function createSquares(num) {
+    for (let i = 1; i <= num; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
     }
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+    });
 }
 
-createSquares();
+function getSquaresPerSide() {
+    let squaresPerSide = Number(prompt("Please insert the number of squares per side for the new grid. The maximum allowed value is 100."));
+    if (!Number.isInteger(squaresPerSide) || squaresPerSide > 100) {
+        alert ("Invalid input.");
+    } else {
+        container.replaceChildren();
+        createSquares((squaresPerSide**2));
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.style.width = (100/squaresPerSide) + "%";
+            square.style.height = (100/squaresPerSide) + "%";
+        });
+    }
+}
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
-});
+createSquares(256);
+
+button = document.querySelector("button");
+button.addEventListener("click", (getSquaresPerSide));
